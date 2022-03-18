b14fe6db6ebda48c9789cc19ba325b5a424c8179
Marius Leahu <MariusLeahu@users.noreply.github.com>
2022-03-18 09:23:26 +0000

    changed gop to tdectl and set create cluster version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='create_cluster',
-    version='0.1.0',
+    version='0.1.1',
     py_modules=['create_cluster'],
     install_requires=[
         'Click', 'boto3', 'pyyaml'
     ],
     entry_points={
         'console_scripts': [
-            'gop = create_cluster:cli',
+            'tdectl = create_cluster:cli',
         ],
     },
 )
